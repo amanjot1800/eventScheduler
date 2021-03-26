c00310f4973bfefe0fa6db9b3f7af9df7499568b
seth0027 <sethi.shivam0624@gmail.com>
2021-03-25 19:03:16 -0500

    commit 2

--- application.py
+++ application.py
@@ -2,18 +2,26 @@ from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://admin:databaseprojectadmin@databaseproject.xy2o1.mongodb.net/event_scheduler"
-mongo = PyMongo(app)
+db = PyMongo(app, uri='mongodb+srv://admin:databaseprojectadmin@databaseproject.xy2o1.mongodb.net/event_scheduler?retryWrites=true&w=majority').db
 
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
     return render_template('index.html')
 
 
-# @app.route('/insert', methods=['POST'])
-# def insert():
-#     form = request.form
-#     mongo.db.events.insert_one(
-#         {'name': form.get('name'), 'time': form.get('time'), 'location': form.get('location'), 'length': form.get('length')})
-#     return 'added'
+@app.route('/insert', methods=['POST'])
+def insert():
+    form = request.form
+    db.events.insert_one(
+        {'name': form.get('name'), 'time': form.get('time'), 'location': form.get('location'), 'length': form.get('length')})
+    return 'added'
+
+
+@app.route('/events')
+def show_events():
+    val = []
+    for event in db.events.find():
+        val.append(event)
+    
