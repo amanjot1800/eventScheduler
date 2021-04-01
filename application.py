5d7e631b5a042685094e3a71469632a1c66351cc
seth0027 <sethi.shivam0624@gmail.com>
2021-04-01 17:53:38 -0500

    /add route

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mongoengine import MongoEngine
 import controller as con
 from event import Event
@@ -14,20 +14,18 @@ app.config['MONGODB_SETTINGS'] = {
 db.init_app(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-
-    return render_template('index.html')
-
-
-@app.route('/insert', methods=['POST'])
-def insert():
-    form = request.form
-    con.create_event(form.get('name'), form.get('date'), form.get('time'), form.get('length'),
-                     form.get('location'), form.get('guests'))
-    return 'added'
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'GET':
+        return render_template('add.html')
+    elif request.method == 'POST':
+        form = request.form
+        con.create_event(form.get('name'), form.get('date'), form.get('time'), form.get('length'),
+                         form.get('location'), form.get('guests'))
+        return redirect(url_for('show_events'))
 
 
+@app.route('/')
 @app.route('/events')
 def show_events():
     return render_template('events.html', events=Event.objects)
